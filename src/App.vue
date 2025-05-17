a535501bcd4ecc9c071487c9cf1d1b9b60356cdd
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-05-17 15:16:00 +0900

    ゲームページ作成

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,12 @@
     <div style="font-family: Meiryo, sans-serif; font-size: 1.5rem;font-weight:bold; color: white;">EMOJI-ESCAPE</div>
     <img alt="タイトル画像2"  src="./assets/escape2.png" width="20%" height="20%" />
   </header>
-
+  <GameLayout />
 </template>
+
+<script setup>
+import GameLayout from './components/GameLayout.vue'
+</script>
 
 <style>
 </style>
---
+++ src/components/GameLayout.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="game-screen">
+    ここがゲーム画面
+  </div>
+</template>
+
+<style scoped>
+.game-screen {
+  background: linear-gradient(90deg, #c2f3f3 0%, #c1e9fc 100%);
+}
+</style>
