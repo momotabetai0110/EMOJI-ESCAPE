506f85e1df96ed94f3d225e13642dcb9497f10e6
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-06-25 22:47:07 +0900

    初dist(docs3回目)

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/EMOJI-ESCAPE/',
   plugins: [
     vue(),
     vueDevTools(),
