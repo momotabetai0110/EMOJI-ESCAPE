9fb9093e2ef0429763af461ef68738755dcec3c5
hiromu terashima <hiromu.terashima@epkotsoftware.co.jp>
2025-08-15 17:54:01 +0900

    dist

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 // src/api/に配置
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    //baseURL: 'http://localhost:8000/api',
+    baseURL:'http://43.207.234.25/api',
     timeout: 30000,//15秒
     headers: {
         'Content-Type': 'application/json'
